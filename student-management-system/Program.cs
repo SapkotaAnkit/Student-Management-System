cd3af70704d22362584bf89fdc354e5d66bfedeb
SapkotaAnkit <ankitsapkota808@gmail.com>
2026-05-21 21:12:22 +0545

    update, delete, search, display added

--- student-management-system/Program.cs
+++ student-management-system/Program.cs
@@ -2,15 +2,64 @@
 {
     public static void Main(String[] args)
     {
-        System.Console.WriteLine("Please Enter the Number on What you want to do: " + "\n1. Add Student" + "\n2. Update Student" + "\n3. Remove Student");
-
-        int choice = Convert.ToInt32(Console.ReadLine());
-
-        if (choice == 1)
+        StudentManager student = new StudentManager();
+        while (true)
         {
-            StudentManager addStudent = new StudentManager();
-            addStudent.AddStudent();
-        }
 
+            System.Console.WriteLine("==========================================");
+            System.Console.WriteLine("Please Enter the Number on What you want to do: " + "\n1. Add Student" + "\n2. Update Student" + "\n3. Remove Student " + "\n4. Display Students" + "\n5. Search Student " + "\n6. Exit ");
+
+            System.Console.WriteLine("==========================================");
+            System.Console.Write("Enter Your Choice: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            if (choice == 1)
+            {
+
+                student.AddStudent();
+            }
+            else if (choice == 4)
+            {
+                System.Console.Write("Enter the ID of the student you want to view: ");
+                string idChoice = Console.ReadLine();
+
+                if (idChoice == "")
+                {
+                    Console.WriteLine("ID cannot be Empty!");
+                    return;
+                }
+                student.DisplayStudents(idChoice);
+            }
+
+            else if (choice == 6)
+            {
+                return;
+            }
+
+            if (choice == 3)
+            {
+                System.Console.Write("Enter the ID of the student you want to remove: ");
+                string deleteStudentId=Console.ReadLine();
+                
+                student.RemoveStudent(deleteStudentId);
+            }
+            if (choice == 2)
+            {
+                System.Console.WriteLine("===============================================");
+                 System.Console.WriteLine("Enter the ID you want to update: ");
+                string updateStudent=Console.ReadLine();
+                student.UpdateStudent(updateStudent);
+
+            }
+            if (choice == 5)
+            {
+                System.Console.WriteLine("==================================================");
+                System.Console.Write("Enter the ID you want to search: ");
+                string searchStudent=Console.ReadLine();
+                student.SearchStudent(searchStudent);
+                
+                
+            }
+        }
     }
 }
--- student-management-system/StudentManager.cs
+++ student-management-system/StudentManager.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography.X509Certificates;
 using System.Transactions;
 
 public class StudentManager
@@ -14,17 +15,17 @@ public class StudentManager
         string ID = Console.ReadLine();
 
         //Enter Name
-        System.Console.Write("Enter Name: ");
+        Console.Write("Enter Name: ");
         string Name = Console.ReadLine();
 
         //Enter Age
 
-        System.Console.Write("Enter Age: ");
+        Console.Write("Enter Age: ");
         int Age = Convert.ToInt32(Console.ReadLine());
 
         //Enter Course
 
-        System.Console.Write("Enter Course: ");
+        Console.Write("Enter Course: ");
         string Course = Console.ReadLine();
 
         // Enter Marks
@@ -37,10 +38,111 @@ public class StudentManager
         // add student to the list
         students.Add(student);
 
-        Student student1 = students.Find(s => s.ID == ID);
+        DisplayStudents(ID);
 
-        System.Console.WriteLine("The student is Added successfully\n" + "ID: " + student1.ID + "\nName: " + student1.Name + "\nAge: " + student1.Age + "\nCourse: " + student1.Course + "\nMarks: " + student1.Marks);
+        System.Console.WriteLine("=================================");
     }
+    public void DisplayStudents(string ID)
+    {
+        Student student = students.Find(s => s.ID == ID);
 
 
+        //if student is null
+        if (student == null)
+        {
+            Console.WriteLine("Error: No student found with that ID.");
+            return;
+        }
+
+        Console.WriteLine("The student is: ");
+        Console.WriteLine("Name: " + student.Name);
+    }
+    public void RemoveStudent(string ID)
+    {
+        Student studentToRemove = students.Find(s => s.ID == ID);
+        bool isDelete = students.Remove(studentToRemove);
+        if (isDelete)
+        {
+            System.Console.WriteLine("It is deleted successfully");
+        }
+        else
+        {
+            System.Console.WriteLine("ID not Found!! ");
+        }
+    }
+    public void UpdateStudent(string ID)
+    {
+        Student studentToUpdate = students.Find(s => s.ID == ID);
+        if (studentToUpdate != null)
+        {
+
+
+            System.Console.WriteLine("Enter the number you want to change: " + "\n1.Name " + "\n2.Age " + "\n3.Course " + "\n4.Marks");
+            string updateChoice = Console.ReadLine();
+            System.Console.WriteLine("===============================================");
+            if (updateChoice == "1")
+            {
+                System.Console.WriteLine("Enter the new Name of the student: ");
+                string newName = Console.ReadLine();
+                if (newName == null)
+                {
+                    System.Console.WriteLine("WTF Nigga enter the damn name!");
+                    return;
+
+                }
+                studentToUpdate.Name = newName;
+
+            }
+            else if (updateChoice == "2")
+            {
+                System.Console.WriteLine("Enter the Age of the student you want to update: ");
+                int newAge = Convert.ToInt32(Console.ReadLine());
+                if (newAge == null)
+                {
+                    System.Console.WriteLine("WTF Nigga enter the damn Age!!");
+                    return;
+                }
+
+            }
+            else if (updateChoice == "3")
+            {
+                System.Console.WriteLine("Enter the course of the student you want to update: ");
+                string newCourse = Console.ReadLine();
+                if (newCourse == null)
+                {
+                    Console.WriteLine("WTF Nigga enter the damn course");
+                    return;
+                }
+            }
+            else if (updateChoice == "4")
+            {
+                System.Console.WriteLine("Enter new marks of the student: ");
+                int newMarks= Convert.ToInt32(Console.ReadLine());
+                if (newMarks == null)
+                {
+                    System.Console.WriteLine("WTF Nigga enter the damn Marks");
+                    return;
+                }
+            }
+            
+            System.Console.WriteLine("Student updated successfully!!");
+        }
+        else
+        {
+            System.Console.WriteLine("Student not found!!");
+        }
+        
+    }
+    public void SearchStudent(string ID)
+    {
+        Student searchStudent =students.Find(s=> s.ID==ID); 
+        if (searchStudent != null)
+                {
+                    System.Console.WriteLine("The student is: ");
+                    System.Console.WriteLine("Name: " + searchStudent.Name);
+                    System.Console.WriteLine("Course: " + searchStudent.Course);
+                    return;
+                }    
+
+    }
 }
